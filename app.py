92df1e4e18d134bc78fb24333f0a5b3149f970d8
Expert632 <machane.salim@gmail.com>
2025-11-22 17:22:04 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+# Fake demo file for secret scanning lab
+
+def connect_to_service():
+    # ❌ Mauvaise pratique volontaire pour le lab
+    api_key = "AKIA_FAKE_DEMO_SECRET_KEY_123456"
+    print("Connecting with API key:", api_key)
+
+if __name__ == "__main__":
+    connect_to_service()
